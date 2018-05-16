d7b2bc570ffc32bcc5957666dd6e1ddaaa158816
Lucas Felix <lfelixsampaio@gmail.com>
2018-05-16 16:22:10 +0000

    [14.17] visualizando estatisticas no hibernate na aplicacao web

--- src/main/java/br/com/caelum/financas/mb/EstatisticasBean.java
+++ src/main/java/br/com/caelum/financas/mb/EstatisticasBean.java
@@ -1,13 +1,33 @@
 package br.com.caelum.financas.mb;
 
+import org.hibernate.Session;
+import org.hibernate.stat.Statistics;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
 @Named
 @ApplicationScoped
 public class EstatisticasBean {
 
-		public void gera() {
-			System.out.println("Gerando estatísticas");
-		}
+    @Inject
+    private EntityManager manager;
+
+    private Statistics estatisticas;
+
+    public void gera() {
+        System.out.println("Gerando estatísticas");
+        Session session = this.manager.unwrap(Session.class);
+        this.estatisticas = session.getSessionFactory().getStatistics();
+    }
+
+    public Statistics getEstatisticas() {
+        return estatisticas;
+    }
+
+    public void setEstatisticas(Statistics estatisticas) {
+        this.estatisticas = estatisticas;
+    }
 }
